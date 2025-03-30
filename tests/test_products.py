ba568eca86e1a05724d018779a5aa91c76517ec7
Fish3r228 <zayarobert30@gmail.com>
2025-03-30 21:36:46 +0300

    work_fixes

--- tests/test_products.py
+++ tests/test_products.py
@@ -2,45 +2,88 @@ import unittest
 from src.products import Product, Category
 
 
+class TestProduct(unittest.TestCase):
+    def setUp(self):
+        self.valid_product = Product("Телефон", "Смартфон", 50000.0, 10)
+
+    def test_product_initialization(self):
+        self.assertEqual(self.valid_product.name, "Телефон")
+        self.assertEqual(self.valid_product.description, "Смартфон")
+        self.assertEqual(self.valid_product.price, 50000.0)
+        self.assertEqual(self.valid_product.quantity, 10)
+
+    def test_product_price_validation(self):
+        with self.assertRaises(TypeError):
+            Product("Телефон", "Смартфон", "не число", 10)
+
+        with self.assertRaises(ValueError):
+            Product("Телефон", "Смартфон", -100.0, 10)
+
+    def test_product_quantity_validation(self):
+        with self.assertRaises(TypeError):
+            Product("Телефон", "Смартфон", 50000.0, "не число")
+
+        with self.assertRaises(ValueError):
+            Product("Телефон", "Смартфон", 50000.0, -5)
+
+    def test_product_name_validation(self):
+        with self.assertRaises(TypeError):
+            Product(12345, "Смартфон", 50000.0, 10)
+
+    def test_product_description_validation(self):
+        with self.assertRaises(TypeError):
+            Product("Телефон", 12345, 50000.0, 10)
+
+    def test_product_str_representation(self):
+        expected_str = "Телефон, Смартфон, 50000.0 руб., Остаток: 10 шт."
+        self.assertEqual(str(self.valid_product), expected_str)
+
+
 class TestCategory(unittest.TestCase):
     def setUp(self):
-        """Сбрасываем счетчики перед каждым тестом"""
         Category.total_categories = 0
         Category.total_products = 0
         self.product1 = Product("Товар1", "Описание1", 1000.0, 5)
         self.product2 = Product("Товар2", "Описание2", 2000.0, 10)
 
-    def test_category_counters(self):
-        """Тест счетчиков при создании категории"""
-        # Создаем и сразу проверяем категорию
-        category = Category("Тестовая", "Описание", [self.product1, self.product2])
+    def test_category_initialization(self):
+        category = Category("Тестовая", "Описание", [self.product1])
         self.assertEqual(category.name, "Тестовая")
         self.assertEqual(category.description, "Описание")
-        self.assertEqual(Category.total_categories, 1)
-        self.assertEqual(Category.total_products, 2)
+        self.assertEqual(len(category.products), 1)
+        self.assertEqual(category.products[0].name, "Товар1")
 
-    def test_empty_category(self):
-        """Тест пустой категори"""
-        # Создаем и проверяем пустую категорию
-        empty_category = Category("Пустая", "Нет товаров", [])
-        self.assertEqual(empty_category.name, "Пустая")
-        self.assertEqual(empty_category.description, "Нет товаров")
-        self.assertEqual(Category.total_categories, 1)
-        self.assertEqual(Category.total_products, 0)
-        self.assertEqual(len(empty_category.products), 0)
+    def test_category_name_validation(self):
+        with self.assertRaises(TypeError):
+            Category(12345, "Описание", [self.product1])
 
-    def test_category_with_single_product(self):
-        """Тест категории с одним товаром"""
-        # Создаем и проверяем категорию с одним товаром
-        single_product = Product("Один", "Единственный", 500.0, 1)
-        single_category = Category("Одиночная", "Один товар", [single_product])
+    def test_category_description_validation(self):
+        with self.assertRaises(TypeError):
+            Category("Тестовая", 12345, [self.product1])
 
-        self.assertEqual(single_category.name, "Одиночная")
-        self.assertEqual(single_category.description, "Один товар")
-        self.assertEqual(Category.total_categories, 1)
-        self.assertEqual(Category.total_products, 1)
-        self.assertEqual(len(single_category.products), 1)
-        self.assertEqual(single_category.products[0].name, "Один")
+    def test_category_products_validation(self):
+        with self.assertRaises(TypeError):
+            Category("Тестовая", "Описание", "не список")
+
+        with self.assertRaises(TypeError):
+            Category("Тестовая", "Описание", [12345])
+
+    def test_category_str_representation(self):
+        category = Category("Тестовая", "Описание", [self.product1])
+        expected_str = (
+            "Тестовая, количество продуктов: 1 шт.\n"
+            "Товар1, Описание1, 1000.0 руб., Остаток: 5 шт."
+        )
+        self.assertEqual(str(category), expected_str)
+
+    def test_category_products_protection(self):
+        category = Category("Защита", "Тест", [self.product1])
+        with self.assertRaises(AttributeError):
+            category.products = [self.product2]
+
+        # Проверяем, что список остался неизменным
+        self.assertEqual(len(category.products), 1)
+        self.assertEqual(category.products[0].name, "Товар1")
 
 
 if __name__ == '__main__':
